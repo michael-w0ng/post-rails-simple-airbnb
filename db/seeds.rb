61b9897e3080d1ed013df19f65f991c22225d805
Michael Wong <mail@michaelwongsilversmith.com>
2018-10-12 17:52:40 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+20.times do |i|
+  puts "Creating flat ##{i}"
+  Flat.create!(
+    name: Faker::HarryPotter.location,
+    address: Faker::GameOfThrones.city,
+    description: Faker::FamousLastWords.last_words,
+    price_per_night: Faker::Number.number(2),
+    number_of_guests: Faker::Number.number(1)
+  )
+end
+
+puts 'Finished!'
